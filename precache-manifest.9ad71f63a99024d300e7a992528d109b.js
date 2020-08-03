0711eef79130ff7e8188d6ef445d2d43a7ab56d5
Simon <mySimonID@gmail.com>
2020-08-03 15:19:10 +0100

    Updates

---
+++ precache-manifest.9ad71f63a99024d300e7a992528d109b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f594adf6860acccfaf0d7a93592c79",
+    "url": "/Text2Doc/index.html"
+  },
+  {
+    "revision": "3aee3be7d06d0ae0db2f",
+    "url": "/Text2Doc/static/css/main.0296d30a.chunk.css"
+  },
+  {
+    "revision": "1486bd585c50d850b877",
+    "url": "/Text2Doc/static/js/2.95f9cc14.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Text2Doc/static/js/2.95f9cc14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aee3be7d06d0ae0db2f",
+    "url": "/Text2Doc/static/js/main.20132e2d.chunk.js"
+  },
+  {
+    "revision": "4f9a2395685fd99163c4",
+    "url": "/Text2Doc/static/js/runtime-main.1be7107f.js"
+  }
+]);
